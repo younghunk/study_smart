09bb9c131889cf55aea87d6f9467287bd45c6ace
c <c@DESKTOP-DB45BAK>
2023-10-19 17:14:58 +0900

    aaaa

---
+++ study/src/main/java/com/example/demo/TestDao.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestDao {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<Map<String, Object>> selectList(Map<String, Object> map) throws Exception {
+        return sqlSession.selectList("test.selectList", map);
+    }
+}
