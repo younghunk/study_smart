dc922eb5738f8cdad9b062081a8b14f95421110a
c <c@DESKTOP-DB45BAK>
2023-10-20 17:06:28 +0900

    sec

---
+++ study/src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SpringSecurityConfig {
+
+     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	http.httpBasic().disable()
+    	.csrf().disable()
+    	.cors().and()
+    	.authorizeHttpRequests()
+    	.antMatchers("/index","/static","/board/**").permitAll();
+        return http.build();
+    }
+}
