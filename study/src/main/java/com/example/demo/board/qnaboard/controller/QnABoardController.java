d211db89c3ede4a7ea7332bd21d0cc2245469ac7
jiwoong <terdy0@hufs.ac.kr>
2023-10-20 17:05:21 +0900

    qqq

--- study/src/main/java/com/example/demo/board/qnaboard/controller/QnABoardController.java
+++ study/src/main/java/com/example/demo/board/qnaboard/controller/QnABoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,6 +15,7 @@ import com.example.demo.board.qnaboard.service.QnaBoardService;
 import com.example.demo.board.vo.PageVo;
 
 @Controller
+@RequestMapping("/board")
 public class QnABoardController {
 
 	@Autowired
