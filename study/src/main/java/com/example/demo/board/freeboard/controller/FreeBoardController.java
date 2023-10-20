09bf1d6cbb2cb8663a8751eb9852d219beff7be8
김유정 <wkdbtlrks321@smartport.kr>
2023-10-20 10:35:37 +0900

    123

--- study/src/main/java/com/example/demo/board/freeboard/controller/FreeBoardController.java
+++ study/src/main/java/com/example/demo/board/freeboard/controller/FreeBoardController.java
@@ -3,6 +3,6 @@ package com.example.demo.board.freeboard.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class FreeBoardController {
+public class FreeBoardController { 
 	 
 }
