d4370ff5eed9a335fac7cc1383cb108f3f804ef9
김유정 <wkdbtlrks321@smartport.kr>
2023-10-20 10:43:05 +0900

    aa123

--- study/src/main/java/com/example/demo/TestController.java
+++ study/src/main/java/com/example/demo/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class TestController {
+public class TestController { 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
