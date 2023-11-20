b88fec37bf586183f36db01f27afd14a641d470b
c <c@DESKTOP-DB45BAK>
2023-11-20 19:56:30 +0900

    11

--- study/src/main/java/com/example/demo/config/SpringSecurityConfig.java
+++ study/src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -1,13 +1,9 @@
 package com.example.demo.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
